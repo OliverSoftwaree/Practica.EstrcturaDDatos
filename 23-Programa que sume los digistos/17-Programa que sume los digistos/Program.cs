779ac9ee12fa830947b5b1ebf62c37a8dc094d17
OliverSoftware <oliverubriveloz24@gmail.com>
2025-05-26 17:04:32 -0400

    Add files via upload

---
+++ 23-Programa que sume los digistos/17-Programa que sume los digistos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un número entero positivo:");
+        string input = Console.ReadLine();
+        
+        if (int.TryParse(input, out int number) && number >= 0)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                sum += number % 10; 
+                number /= 10;       
+            }
+            Console.WriteLine($"La suma de los dígitos es: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Por favor, ingrese un número entero positivo válido.");
+        }
+    }
+}
