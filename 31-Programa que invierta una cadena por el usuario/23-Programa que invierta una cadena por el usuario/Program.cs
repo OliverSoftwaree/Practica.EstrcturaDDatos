bdc04f9fc30635f257cc39c52f10039008a9a7ad
OliverSoftware <oliverubriveloz24@gmail.com>
2025-05-26 17:19:27 -0400

    Add files via upload

---
+++ 31-Programa que invierta una cadena por el usuario/23-Programa que invierta una cadena por el usuario/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese una cadena de texto:");
+        string input = Console.ReadLine();
+        
+        if (input != null)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedString = new string(charArray);
+            
+            Console.WriteLine("Cadena invertida: " + reversedString);
+        }
+        else
+        {
+            Console.WriteLine("No se ingresó ninguna cadena.");
+        }
+    }
+}
