79326db5f5bc41e4ba6fe2251613f37391f248c3
OliverSoftware <oliverubriveloz24@gmail.com>
2025-05-26 17:08:43 -0400

    Add files via upload

---
+++ 15-Programa que diga si es un numero perfecto o no/Programa que diga si es un numero perfecto o no/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número: ");
+        if (int.TryParse(Console.ReadLine(), out int numero))
+        {
+            if (EsNumeroPerfecto(numero))
+            {
+                Console.WriteLine($"{numero} es un número perfecto.");
+            }
+            else
+            {
+                Console.WriteLine($"{numero} no es un número perfecto.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada no válida. Por favor, ingrese un número entero.");
+        }
+    }
+    static bool EsNumeroPerfecto(int numero)
+    {
+        int sumaDivisores = 0;
+        for (int i = 1; i < numero; i++)
+        {
+            if (numero % i == 0)
+            {
+                sumaDivisores += i;
+            }
+        }
+        return sumaDivisores == numero;
+    }
+}
