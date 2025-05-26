f146a4227724140b88d415e2a5358f7456f21416
OliverSoftware <oliverubriveloz24@gmail.com>
2025-05-26 17:11:32 -0400

    Add files via upload

---
+++ 22-Programa que calcule los digitos  de un numero/16-Programa que calcule los digitos  de un numero/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un número entero:");
+        string input = Console.ReadLine();
+        
+        if (int.TryParse(input, out int number))
+        {
+            int digitCount = 0;
+            int temp = Math.Abs(number); 
+            do
+            {
+                digitCount++;
+                temp /= 10;
+            } while (temp > 0);
+            Console.WriteLine($"El número {number} tiene {digitCount} dígitos.");
+        }
+        else
+        {
+            Console.WriteLine("Entrada no válida. Por favor, ingrese un número entero.");
+        }
+    }
+}
