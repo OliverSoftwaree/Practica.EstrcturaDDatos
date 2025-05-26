d54a2a9d2e3656c8ae38a45b6ed68f5830f0239e
OliverSoftware <oliverubriveloz24@gmail.com>
2025-05-26 17:03:48 -0400

    Add files via upload

---
+++ 20-Factotial de un numero/14-Factotial de un numero/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un numero para calcular su factorial: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        int factorial = 1;
+        for (int i = 1; i <= numero; i++)
+        {
+            factorial *= i;
+        }
+        Console.WriteLine($"El factorial de {numero} es: {factorial}");
+    }
+}
---
+++ 21-Programa que determine si es un num. Capicua/15-Programa que determine si es un num. Capicua/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un número entero:");
+        string input = Console.ReadLine();
+        
+        if (IsCapicua(input))
+        {
+            Console.WriteLine("El número es capicúa.");
+        }
+        else
+        {
+            Console.WriteLine("El número no es capicúa.");
+        }
+    }
+    static bool IsCapicua(string number)
+    {
+    
+        string cleanedNumber = number.Replace(" ", "").ToLower();
+        
+        char[] charArray = cleanedNumber.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedNumber = new string(charArray);
+        
+        return cleanedNumber == reversedNumber;
+    }
+}
+
